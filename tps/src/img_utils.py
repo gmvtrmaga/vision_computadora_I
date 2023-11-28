ea1fc989e2a08b16cac19e4f7eafdc2574c4c5f9
gmvtrmaga <margomez@gmv.com>
2023-11-28 01:39:08 -0300

    refactorization

--- tps/src/img_utils.py
+++ tps/src/img_utils.py
@@ -1,49 +1,48 @@
 from cv2 import COLOR_BGR2RGB, cvtColor, imread, merge, split
-from numpy import float64, maximum
-from numpy import max as np_max
+from numpy import float64, max, maximum
 
 
-def cargar_en_RGB(path: str):
+def load_as_RGB(path: str):
     ret = imread(path)
     return cvtColor(ret, COLOR_BGR2RGB)
 
 
-def tform_coord_cromaticas(img):
-    # Separar canales de color y transformarlos a float
+def tform_crom_coord(img):
+    # Split image in RGB channels and set them to float to prepare them for arithmetic operations
     red_channel, green_channel, blue_channel = split(img)
 
     red_channel = red_channel.astype(float64)
     green_channel = green_channel.astype(float64)
     blue_channel = blue_channel.astype(float64)
 
-    # Aplicar formula f(RGB) = (R/(R+G+B), G/(R+G+B), B(R+G+B) )
-    # Evitamos dividir entre 0 con un valor epsilon
+    # Apply formula f(RGB) = (R/(R+G+B), G/(R+G+B), B(R+G+B) )
+    # Avoid zero division using epsilon
     tot = maximum(red_channel + green_channel + blue_channel, 1e-10)
 
     red_channel = red_channel / tot
     green_channel = green_channel / tot
     blue_channel = blue_channel / tot
 
-    # Fusionar los canales de nuevo en una imagen
+    # Merge channels to image
     return merge([red_channel, green_channel, blue_channel])
 
 
 def tform_white_patch(img):
-    # Separar canales de color y transformarlos a float
+    # Split image in RGB channels and set them to float to prepare them for arithmetic operations
     red_channel, green_channel, blue_channel = split(img)
 
     red_channel = red_channel.astype(float64)
     green_channel = green_channel.astype(float64)
     blue_channel = blue_channel.astype(float64)
 
-    # Aplicar algoritmo white patch
-    red_max = np_max(red_channel)
-    green_max = np_max(green_channel)
-    blue_max = np_max(blue_channel)
+    # Apply white patch algorithm
+    red_max = max(red_channel)
+    green_max = max(green_channel)
+    blue_max = max(blue_channel)
 
     red_channel = red_channel / red_max
     green_channel = green_channel / green_max
     blue_channel = blue_channel / blue_max
 
-    # Fusionar los canales de nuevo en una imagen
+    # Merge channels to image
     return merge([red_channel, green_channel, blue_channel])
