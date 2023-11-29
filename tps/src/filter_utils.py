c3de8dd1d910e4a43b715237f97d71ace65a25ff
gmvtrmaga <margomez@gmv.com>
2023-11-29 02:57:57 -0300

    implemented filter application

--- tps/src/filter_utils.py
+++ tps/src/filter_utils.py
@@ -1,5 +1,5 @@
 from cv2 import getGaussianKernel
-from numpy import outer, fft
+from numpy import fft, outer, real
 
 
 def create_gauss_filter(height, width, k_size, sigma):
@@ -13,6 +13,16 @@ def create_gauss_filter(height, width, k_size, sigma):
     gauss_kernel /= gauss_kernel.sum()
 
     # Get fourier transformation of the kernel matching the img size
-    gauss_fft = fft.fft2(gauss_kernel, s=(height, width))
+    return gauss_kernel, img_to_fourier(gauss_kernel, height, width)
 
-    return gauss_kernel, gauss_fft
+
+def img_to_fourier(img, height, width):
+    # Get fourier transformation of the img
+    img_fft = fft.fft2(img, s=(height, width))
+    return fft.fftshift(img_fft)
+
+
+def fourier_to_img(img):
+    # Inverse functions
+    f_ishift = fft.ifftshift(img)
+    return real(fft.ifft2(f_ishift))
