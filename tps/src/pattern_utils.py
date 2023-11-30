cf823e9255f93457e55705b27af32f4d95f6869d
gmvtrmaga <margomez@gmv.com>
2023-11-30 15:50:32 -0300

    print detection with text

--- tps/src/pattern_utils.py
+++ tps/src/pattern_utils.py
@@ -2,13 +2,16 @@ from cv2 import (
     COLOR_BGR2HSV,
     COLOR_BGR2RGB,
     COLOR_RGB2GRAY,
+    FONT_HERSHEY_SIMPLEX,
     IMREAD_GRAYSCALE,
     TM_CCOEFF_NORMED,
     Canny,
     GaussianBlur,
     cvtColor,
+    getTextSize,
     imread,
     merge,
+    putText,
     split,
     resize,
     matchTemplate,
@@ -16,7 +19,7 @@ from cv2 import (
     groupRectangles,
     minMaxLoc,
 )
-from numpy import float64, max, maximum, where
+from numpy import clip, float64, max, maximum, uint8, where
 
 
 def tform_gauss_border_filter(img, kernel_size, th1, th2, contrast_factor=2.0):
@@ -25,34 +28,53 @@ def tform_gauss_border_filter(img, kernel_size, th1, th2, contrast_factor=2.0):
         kernel_size = kernel_size + 1
     img_gray = GaussianBlur(img_gray, (kernel_size, kernel_size), 0)
     img_canny = Canny(img_gray, threshold1=th1, threshold2=th2)
-    return img_canny * contrast_factor
+
+    return clip(img_canny * contrast_factor, 0, 255).astype(uint8)
 
 
-def detect_single_pattern(template, img, img_original, template_scale=1.0):
-    # template_gray = cvtColor(template, COLOR_RGB2GRAY)
-    # img_gray = cvtColor(img, COLOR_RGB2GRAY)
-    template_gray = template
-    img_gray = img
-
-    # Template dimensions
-    h, w = template_gray.shape
-
+def detect_single_pattern(template, img, template_scale=1.0):
     # Rescale template if needed
     if template_scale != 1.0:
-        h, w = int(h * template_scale), int(w * template_scale)
-        template_gray = resize(template_gray, (h, w))
+        template = resize(template, None, fx=template_scale, fy=template_scale)
+
+    # Template dimensions
+    h, w = template.shape
 
     # Apply template detection
-    res = matchTemplate(img_gray, template_gray, TM_CCOEFF_NORMED)
+    res = matchTemplate(img, template, TM_CCOEFF_NORMED)
 
     # Find the location of the best match
-    _, _, _, max_loc = minMaxLoc(res)
+    _, max_corr, _, max_loc = minMaxLoc(res)
 
     # Draw bounding box on the original image
     x, y = max_loc
     x2, y2 = x + w, y + h
 
-    result_image = img_original.copy()
-    result_image = rectangle(result_image, (x, y), (x2, y2), (0, 255, 0), 2)
+    return [(x, y, x2, y2)], [max_corr]
 
-    return result_image, [(x, y, x2, y2)]
+
+def paint_detections(img, rectangles, corrs, border_size=2):
+    TEXT_OFFSET = 3
+    ret = img.copy()
+    nItems = min(len(rectangles), len(corrs))
+    for i in range(nItems):
+        x, y, x2, y2 = rectangles[i]
+        p_corr = f"{round(corrs[i] * 100, 1)}%"
+        (label_width, label_height), _ = getTextSize(p_corr, FONT_HERSHEY_SIMPLEX, 1, 2)
+        text_end = (x + label_width, y - label_height - 2 * border_size)
+
+        ret = rectangle(ret, (x, y), (x2, y2), (0, 255, 0), border_size)
+        ret = rectangle(ret, (x, y), text_end, (0, 255, 0), -1)
+        ret = rectangle(ret, (x, y), text_end, (0, 255, 0), border_size)
+
+        ret = putText(
+            ret,
+            p_corr,
+            (x - TEXT_OFFSET, y - TEXT_OFFSET),
+            FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 0),
+            2,
+        )
+
+    return ret
